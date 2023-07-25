bc5f68b1c745fdf2733bde66ace2ef2fc01971e1
Brad Sickles <brad.sickles@gmail.com>
2023-07-25 14:05:07 -0400

    Create isolated cluster if a preview env

--- cluster.tf
+++ cluster.tf
@@ -4,7 +4,44 @@ data "ns_connection" "cluster" {
 }
 
 locals {
-  cluster_arn    = data.ns_connection.cluster.outputs.cluster_arn
-  cluster_name   = data.ns_connection.cluster.outputs.cluster_name
-  deployers_name = data.ns_connection.cluster.outputs.deployers_name
+  capacity_provider_name = data.ns_connection.cluster.outputs.capacity_provider_name
+
+  cluster_arn    = local.is_preview_env ? aws_ecs_cluster.namespace[0].arn : data.ns_connection.cluster.outputs.cluster_arn
+  cluster_name   = local.is_preview_env ? aws_ecs_cluster.namespace[0].name : data.ns_connection.cluster.outputs.cluster_name
+  deployers_name = local.is_preview_env ? aws_iam_group.deployers[0].name : data.ns_connection.cluster.outputs.deployers_name
+}
+
+resource "aws_ecs_cluster" "namespace" {
+  count = local.is_preview_env ? 1 : 0
+
+  name = local.resource_name
+  tags = local.tags
+
+  // TODO: Enable execute command with encryption configured on logging
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "namespace" {
+  count = local.is_preview_env ? 1 : 0
+
+  cluster_name       = aws_ecs_cluster.namespace[count.index].name
+  capacity_providers = ["FARGATE"]
+}
+
+
+resource "aws_ecs_cluster_capacity_providers" "this" {
+  count = local.is_preview_env ? 1 : 0
+
+  cluster_name       = aws_ecs_cluster.namespace.name
+  capacity_providers = [local.capacity_provider_name]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = local.capacity_provider_name
+  }
 }
